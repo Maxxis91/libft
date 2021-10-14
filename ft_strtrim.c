7fa6781d9812d7e8c22e2e7ea095d7209ad8b4de
Gastly Melissia <gmelissi@ev-f3.kzn.21-school.ru>
2021-10-14 19:24:36 +0300

    ft_strtrim norm OK

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gmelissi <gmelissi@student.21-schoo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 18:10:35 by gmelissi          #+#    #+#             */
-/*   Updated: 2021/10/14 19:17:47 by gmelissi         ###   ########.fr       */
+/*   Updated: 2021/10/14 19:24:03 by gmelissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,32 +18,25 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	m;
 	size_t	n;
 
-	if (s1)
+	if (!s1)
+		return (NULL);
+	m = 0;
+	n = ft_strlen(s1);
+	if (set)
 	{
-		m = 0;
-		n = ft_strlen(s1);
-		if (set)
-		{
-			while (ft_strchr(set, *(s1 + m)) && m <= ft_strlen(s1))
-				m++;
-			while (ft_strchr(set, *(s1 + n - 1)) && n > 0)
-				n--;
-		}
-		if (m > n)
-		{
-			res = (char *)malloc(1);
-			if (!res)
-				return (NULL);
-			*res = '\0';
-		}
-		else
-		{
-			res = (char *)malloc(n - m + 1);
-			if (!res)
-				return (NULL);
-			(void)ft_strlcpy(res, s1 + m, n - m + 1);
-		}
-		return (res);
+		while (ft_strchr(set, *(s1 + m)) && m <= ft_strlen(s1))
+			m++;
+		while (ft_strchr(set, *(s1 + n - 1)) && n > 0)
+			n--;
 	}
-	return (NULL);
+	if (m > n)
+		m = n;
+	res = (char *)malloc(n - m + 1);
+	if (!res)
+		return (NULL);
+	if (m - n)
+		(void)ft_strlcpy(res, s1 + m, n - m + 1);
+	else
+		*res = '\0';
+	return (res);
 }
