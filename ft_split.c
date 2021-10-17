cb62d65e863021edc04ad56cdaa0f58c015e6f06
Gastly Melissia <gmelissi@ev-c1.kzn.21-school.ru>
2021-10-17 15:59:13 +0300

    ft_split ok

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gmelissi <gmelissi@student.21-schoo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 19:57:41 by gmelissi          #+#    #+#             */
-/*   Updated: 2021/10/17 15:47:17 by gmelissi         ###   ########.fr       */
+/*   Updated: 2021/10/17 15:58:29 by gmelissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,15 @@
 static size_t	ft_wordcnt(char const *s, char c)
 {
 	size_t	res;
-	char	*tr;
 
 	res = 0;
-	tr = (char *)s;
-	if (!tr || !*tr)
+	if (!s || !*s)
 		return (res);
-	while (*tr)
+	while (*s)
 	{
-		if (*tr != c && (*(tr + 1) == c || !*(tr + 1)))
+		if (*s != c && (*(s + 1) == c || !*(s + 1)))
 			res++;
-		tr++;
+		s++;
 	}
 	return (res);
 }
@@ -33,16 +31,14 @@ static size_t	ft_wordcnt(char const *s, char c)
 static size_t	ft_wordlen(char const *s, char c)
 {
 	size_t	len;
-	char	*r;
 
 	len = 0;
-	r = (char *)s;
-	while (*r == c)
-		r++;
-	while (*r && *r != c)
+	while (*s == c)
+		s++;
+	while (*s && *s != c)
 	{
 		len++;
-		r++;
+		s++;
 	}
 	return (len);
 }
@@ -63,37 +59,31 @@ static char	**ft_free_2d(char **ar)
 	return (ar);
 }
 
-
 char	**ft_split(char const *s, char c)
 {
 	char	**res;
-	char	*r;
 	size_t	cnt;
 	size_t	len;
 	size_t	i;
 
 	if (!s)
 		return (NULL);
-	r = (char *)s;
-	cnt = ft_wordcnt(r, c);
+	cnt = ft_wordcnt(s, c);
 	res = (char **)malloc(sizeof(char *) * (cnt + 1));
 	if (!res)
 		return (NULL);
 	i = 0;
-	len = 0;
 	while (i < cnt)
 	{
-		len = ft_wordlen(r, c) + 1;
-		while (*r == c)
-			r++;
+		len = ft_wordlen(s, c) + 1;
+		while (*s == c)
+			s++;
 		res[i] = (char *)malloc(len);
 		if (!res[i])
 			return (ft_free_2d(res));
-		ft_strlcpy(res[i], r, len);
-		r += len;
-		i++;
+		ft_strlcpy(res[i++], s, len);
+		s += len;
 	}
 	res[i] = NULL;
 	return (res);
 }
-
